48a33650e8c25ba7c67644b10cdc9de9a8127dac
Gonul Gulsen <108784267+AllyGulsen@users.noreply.github.com>
2023-01-11 11:35:28 +0300

    Add files via upload

---
+++ LCW_Project_SeleniumPOM_TestNG/src/test/java/POM/ProductPage_Elements.java
@@ -0,0 +1,22 @@
+package POM;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage_Elements {
+
+    public ProductPage_Elements(WebDriver driver){PageFactory.initElements(driver,this);}
+
+    @FindBy(css="a[data-optionid='2559039']>div")
+    public WebElement cicekDesenliSal;
+
+    @FindBy(css = "a[size='Standart']")
+    public WebElement standartSize;
+
+    @FindBy(xpath = "(//a[text()='SEPETE EKLE'])[2]")
+    public WebElement starWarsCantaAddToCartButton;
+
+
+}
